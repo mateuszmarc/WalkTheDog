28a3893a8ff31d274237c7898ab53d57caa380ad
mateuszmarc <mmarckiewicz@gmail.com>
2024-07-03 21:23:29 +0000

    Initialize spring project. Set up data source and logging.

---
+++ src/main/java/com/mateuszmarcyk/walk_the_dog/WalkTheDogApplication.java
@@ -0,0 +1,13 @@
+package com.mateuszmarcyk.walk_the_dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkTheDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkTheDogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateuszmarcyk/walk_the_dog/WalkTheDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszmarcyk.walk_the_dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkTheDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
