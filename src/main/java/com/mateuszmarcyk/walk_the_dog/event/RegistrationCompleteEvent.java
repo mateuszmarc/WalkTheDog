9e57e306c5f763d020f2a15cfa9e71277bdc5752
mateuszmarc <mmarckiewicz@gmail.com>
2024-07-04 07:14:18 +0000

    Create RegistrationCompleteEvent class

--- src/main/java/com/mateuszmarcyk/walk_the_dog/event/RegistrationCompleteEvent.java
+++ src/main/java/com/mateuszmarcyk/walk_the_dog/event/RegistrationCompleteEvent.java
@@ -1,4 +1,20 @@
 package com.mateuszmarcyk.walk_the_dog.event;
 
-public class RegistrationCompleteEvent {
+import com.mateuszmarcyk.walk_the_dog.model.AppUser;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+@Setter
+public class RegistrationCompleteEvent extends ApplicationEvent {
+
+    private AppUser appUser;
+    private String applicationUrl;
+
+    public RegistrationCompleteEvent(AppUser appUser, String applicationUrl) {
+        super(appUser);
+        this.appUser = appUser;
+        this.applicationUrl = applicationUrl;
+    }
 }
