96a99262812802b61309b3a3af77ad955ec1f9c8
mateuszmarc <mmarckiewicz@gmail.com>
2024-07-07 11:49:20 +0000

    Change SecurityConfig to test login form. Add thymeleaf congiguration to properties

--- src/main/java/com/mateuszmarcyk/walkthedog/security/UserRegistrationSecurityConfig.java
+++ src/main/java/com/mateuszmarcyk/walkthedog/security/UserRegistrationSecurityConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -26,20 +30,32 @@ public class UserRegistrationSecurityConfig {
                 .cors(withDefaults())
                 .csrf(configurer -> configurer.disable())
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                                .requestMatchers("/**").permitAll());
+                        .requestMatchers("/**").permitAll()
+
 //                        .requestMatchers("/register/**").permitAll()
 //                        .requestMatchers("/users/**").hasAnyAuthority("USER", "ADMIN")
-//                ).formLogin();
-//                .formLogin(formLogin -> formLogin
-//                        .loginPage("/login")
-//                        .loginProcessingUrl("/process")
-//                        .defaultSuccessUrl("/users")
-//                        .permitAll()
-//                ).logout(logout ->
-//                        logout.permitAll()
-//                );
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/users")
+                        .permitAll()
+                ).logout(logout ->
+                        logout.permitAll()
+                );
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 
 
