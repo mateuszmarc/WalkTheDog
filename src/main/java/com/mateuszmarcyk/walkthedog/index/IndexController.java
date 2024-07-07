dd14b9e6350b227307004aa04034f4da47cda941
mateuszmarc <mmarckiewicz@gmail.com>
2024-07-07 09:18:02 +0000

    Change context path to start with "/walkthedog". Create IndexController to display index page and about page Refactor index view to derive contact section. Add href in anchor tags. C#	nowy plik:       src/main/resources/templates/about.html

--- src/main/java/com/mateuszmarcyk/walkthedog/index/IndexController.java
+++ src/main/java/com/mateuszmarcyk/walkthedog/index/IndexController.java
@@ -2,14 +2,23 @@ package com.mateuszmarcyk.walkthedog.index;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/walkthedog")
 public class IndexController {
 
     @GetMapping
     public String displayIndexPage() {
         return "index";
     }
+
+    @GetMapping("/about")
+    public String displayAboutPage() {
+        return "about";
+    }
+
+    @GetMapping("/contat")
+    public String displayContactPage() {
+        return "contact";
+    }
+
 }
